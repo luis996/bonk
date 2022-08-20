c6702f8740187466f6b44b1563c8398f765f0eb7
luis996 <52387429+luis996@users.noreply.github.com>
2022-08-19 22:29:39 -0500

    added basic bot

---
+++ index.ts
@@ -0,0 +1,24 @@
+import DiscordJS from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new DiscordJS.Client({
+  intents: [3276799]
+})
+
+client.on('ready', () => {
+  console.log('Sending debug info')
+  console.log(process.env.token)
+  console.log(process.env.MONGO_URI)
+})
+
+client.on('messageCreate', (message) => {
+  if (message.content === 'ping') {
+    console.log('input: ping, output: pong')
+    message.reply({
+      content: '',
+    })
+  }
+})
+
+client.login(process.env.token)
