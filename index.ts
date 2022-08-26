9ee6bf84a4b7124488cc27d3c16c4e8fe2ad57e5
luis996 <52387429+luis996@users.noreply.github.com>
2022-08-25 20:39:13 -0500

    add ping command

--- index.ts
+++ index.ts
@@ -9,5 +9,29 @@ const client = new DiscordJS.Client({
 client.on('ready', () => {
     client.user?.setPresence({ activities: [{ name: '/help' }], status: 'online' });
     console.log('bot encendido')
+
+    const guildId = '1009243706538348544'
+    const guild = client.guilds.cache.get(guildId)
+    let commands
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+    commands?.create({
+        name:'ping',
+        description: 'Replies with pong',
+    })
+})
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) {
+        return
+    }
+    const { commandName, options, } = interaction
+    if (commandName === 'ping') {
+        interaction.reply({
+            content: 'Pong!',
+        })
+    }
 })
 client.login(process.env.token)
